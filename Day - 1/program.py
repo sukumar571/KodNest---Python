6ac8e2094b8b56455242b784bd3f2bc7260383ef
sukumar571 <suku0571@gmail.com>
2026-07-08 10:24:28 +0530

    mine

---
+++ Day - 1/program.py
@@ -0,0 +1,17 @@
+print("Hello, Sukumar!")
+
+print("Thank you", end=" - ")
+print("Bye")
+
+print("KodNest \n Institute")
+
+
+
+for i in range(10):
+    print(i)
+
+i = int(input("Enter the value of i: "))
+if i == i:
+    print("Equals")
+else:
+    print("Not equals")
